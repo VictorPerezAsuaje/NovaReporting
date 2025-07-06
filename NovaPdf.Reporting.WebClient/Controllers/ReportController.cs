0d38d12ce6e768de193d740e73b45df6dd194989
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2025-07-06 10:42:18 +0200

    Feat: Added a preview approach to static HTML alternative to razor for Reports

--- NovaPdf.Reporting.WebClient/Controllers/ReportController.cs
+++ NovaPdf.Reporting.WebClient/Controllers/ReportController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using NovaPdf.Reporting.Core;
 using NovaPdf.Reporting.Razor;
 using NovaPdf.Reporting.WebClient.Reports.Sales;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace NovaPdf.Reporting.WebClient.Controllers;
 
@@ -58,14 +60,110 @@ public class ReportController : Controller
         return File(result.Value, "application/pdf", "sample.pdf");
     }
 
+    [HttpGet]
+    [Route("preview-static-sales")]
+    public async Task<IActionResult> PreviewStaticSales()
+    {
+        SalesNovaReport salesReport = new SalesNovaReport(HttpContext.RequestServices);
+        string currentDir = Directory.GetCurrentDirectory();
+        string reportFolder = Path.Combine(currentDir, "StaticReports", salesReport.Name);
+        string layout = System.IO.File.ReadAllText(Path.Combine(reportFolder, "layout.html"));
+
+        layout = layout.Replace("{{ BODY_CLASSES }}", "d-flex flex-column align-items-center justify-content-center min-vh-100");
+        layout = layout.Replace("{{ BODY_STYLES }}", "background-color:#f1f1f1;");
+        layout = layout.Replace("{{ CONTAINER_CLASSES }}", "pt-4 bg-white");
+        layout = layout.Replace("{{ CONTAINER_STYLES }}", $"width: {(salesReport.Layout == ReportLayout.Vertical ? salesReport.Format.Width : salesReport.Format.Height)}mm; min-height: 95vh;");
+        layout = layout.Replace("{{ REPORT_CONTAINER_CLASSES }}", "p-4");
+
+        string header = System.IO.File.ReadAllText(Path.Combine(reportFolder, "header.html"));
+        foreach (var param in salesReport.Parameters)
+            header = header.Replace($"{{{{ {param.Key} }}}}", $"{param.Value.Value}");
+
+        string salesSumaryRowsTemplate = "";
+        string saleRowTemplate = System.IO.File.ReadAllText(Path.Combine(reportFolder, "sale-row.html"));
+
+        decimal lastTotalSales = 0;
+        decimal lastReturns = 0;
+        decimal lastNewCustomers = 0;
+        SalesSummaryDS salesDS = salesReport.GetDataSet<SalesSummaryDS>();
+
+        foreach (var item in salesDS.Data)
+        {
+            string saleRow = saleRowTemplate;
+            bool upSales = item.TotalSales > lastTotalSales;
+            bool upReturns = item.TotalReturns > lastReturns;
+            bool upNewCustomers = item.NewCustomers > lastNewCustomers;
+            lastTotalSales = item.TotalSales;
+            lastReturns = item.NetSales;
+            lastNewCustomers = item.NewCustomers;
+
+            saleRow = saleRow.Replace("{{ Month }}", $"{item.Month}");
+            saleRow = saleRow.Replace("{{ TotalSales }}", $"{item.TotalSales}");
+            saleRow = saleRow.Replace("{{ TotalSalesIconClass }}", upSales ? "fa-caret-up text-success" : "fa-caret-down text-danger");
+            saleRow = saleRow.Replace("{{ TotalReturns }}", $"{item.TotalReturns}");
+            saleRow = saleRow.Replace("{{ TotalReturnsIconClass }}", upReturns ? "fa-caret-up text-success" : "fa-caret-down text-danger");
+            saleRow = saleRow.Replace("{{ NetSales }}", $"{item.NetSales}");
+            saleRow = saleRow.Replace("{{ NewCustomers }}", $"{item.NewCustomers}");
+            saleRow = saleRow.Replace("{{ NewCustomersIconClass }}", upNewCustomers ? "fa-caret-up text-success" : "fa-caret-down text-danger");
+            salesSumaryRowsTemplate += saleRow;
+        }
+
+        string totaSaleRowTemplate = System.IO.File.ReadAllText(Path.Combine(reportFolder, "total-sale-row.html"));
+        totaSaleRowTemplate = totaSaleRowTemplate.Replace("{{ SalesTargetIconClass }}", $"{(salesDS.SumTotalSales < salesReport.GetParameter<decimal>("SalesTarget") ? "bg-danger" : "bg-success")}");
+        totaSaleRowTemplate = totaSaleRowTemplate.Replace("{{ SumTotalSales }}", $"{salesDS.SumTotalSales.ToString("N2")}");
+        totaSaleRowTemplate = totaSaleRowTemplate.Replace("{{ SalesTarget }}", $"{salesReport.GetParameter<decimal>("SalesTarget").ToString("N2")}");
+        totaSaleRowTemplate = totaSaleRowTemplate.Replace("{{ SumTotalReturns }}", $"{salesDS.SumTotalReturns.ToString("N2")}");
+        totaSaleRowTemplate = totaSaleRowTemplate.Replace("{{ SumNetSales }}", $"{salesDS.SumNetSales.ToString("N2")}");
+        totaSaleRowTemplate = totaSaleRowTemplate.Replace("{{ SumNewCustomers }}", $"{salesDS.SumNewCustomers}");
+        salesSumaryRowsTemplate += totaSaleRowTemplate;
+
+        string productPerformanceRowsTemplate = "";
+        string productRowTemplate = System.IO.File.ReadAllText(Path.Combine(reportFolder, "product-row.html"));
+        ProductPerformanceDS productDS = salesReport.GetDataSet<ProductPerformanceDS>();
+        foreach (var item in productDS.Data)
+        {
+            string productRow = productRowTemplate;
+            bool isTopPerforming = item.ProductId == productDS.TopPerformingProductId;
+
+            productRow = productRow.Replace("{{ ProductId }}", $"{item.ProductId}");
+            productRow = productRow.Replace("{{ TopPerformingIcon }}", isTopPerforming ? $"<i class=\"ms-2 fa-solid fa-star text-warning\"></i>" : "");
+
+            productRow = productRow.Replace("{{ ProductName }}", $"{item.ProductName}");
+            productRow = productRow.Replace("{{ UnitsSold }}", $"{item.UnitsSold}");
+            productRow = productRow.Replace("{{ Revenue }}", $"{item.Revenue}");
+            productRow = productRow.Replace("{{ AvgPrice }}", $"{item.AvgPrice}");
+            productRow = productRow.Replace("{{ PercentageReturnRate }}", $"{item.PercentageReturnRate}");
+            productPerformanceRowsTemplate += productRow;
+        }
+
+        string report = System.IO.File.ReadAllText(Path.Combine(reportFolder, "report.html"));
+
+        report = report.Replace("{{ SalesSummaryRows }}", salesSumaryRowsTemplate);
+        report = report.Replace("{{ ProductPerformanceRows }}", productPerformanceRowsTemplate);
+
+        foreach (var param in salesReport.Parameters)
+            report = report.Replace($"{{{{ {param.Key} }}}}", $"{param.Value.Value}");
+
+        layout = layout.Replace("{{ HEADER }}", header);
+        layout = layout.Replace("{{ REPORT }}", report);
+
+        return Content(layout, "text/html");
+    }
+
     [HttpPost]
     [Route("static-sales")]
     public async Task<IActionResult> DownloadStaticSales()
     {
         SalesNovaReport salesReport = new SalesNovaReport(HttpContext.RequestServices);
-        
         string currentDir = Directory.GetCurrentDirectory();
         string reportFolder = Path.Combine(currentDir, "StaticReports", salesReport.Name);
+        string layout = System.IO.File.ReadAllText(Path.Combine(reportFolder, "layout.html"));
+
+        layout = layout.Replace("{{ BODY_CLASSES }}", "p-4");
+        layout = layout.Replace("{{ BODY_STYLES }}", "");
+        layout = layout.Replace("{{ CONTAINER_CLASSES }}", "");
+        layout = layout.Replace("{{ CONTAINER_STYLES }}", "");
+        layout = layout.Replace("{{ REPORT_CONTAINER_CLASSES }}", "");
 
         string header = System.IO.File.ReadAllText(Path.Combine(reportFolder, "header.html"));
 
@@ -137,10 +235,13 @@ public class ReportController : Controller
         foreach (var param in salesReport.Parameters)
             report = report.Replace($"{{{{ {param.Key} }}}}", $"{param.Value.Value}");
 
+        layout = layout.Replace("{{ HEADER }}", "");
+        layout = layout.Replace("{{ REPORT }}", report);
+
         var result = await salesReport.GeneratePdfAsync(x =>
         {
             x.Header = header;
-            x.Html = report;
+            x.Html = layout;
             x.MarginTop = 175;
         });
 
